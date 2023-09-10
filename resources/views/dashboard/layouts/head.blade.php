449e566bdd2f5c6cbcf82ec7cf518f0a9d5827d8
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2023-09-10 15:02:21 +0300

    install laravel & all packages

---
+++ resources/views/dashboard/layouts/head.blade.php
@@ -0,0 +1,22 @@
+<!-- Title -->
+<title> Valex -  Premium dashboard ui bootstrap rwd admin html5 template </title>
+<!-- Favicon -->
+<link rel="icon" href="{{asset('build/assets/dashboard/img/brand/favicon.png')}}" type="image/x-icon"/>
+<!-- Icons css -->
+<link href="{{asset('build/assets/dashboard/css/icons.css')}}" rel="stylesheet">
+<!--  Custom Scroll bar-->
+<link href="{{asset('build/assets/dashboard/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
+<!--  Right-sidemenu css -->
+<link href="{{asset('build/assets/dashboard/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
+<!-- Sidemenu css -->
+<link rel="stylesheet" href="{{asset('build/assets/dashboard/css/sidemenu.css')}}">
+@yield('css')
+<!-- Maps css -->
+<link href="{{asset('build/assets/dashboard/plugins/jqvmap/jqvmap.min.css')}}" rel="stylesheet">
+<!-- style css -->
+<link href="{{asset('build/assets/dashboard/css/style.css')}}" rel="stylesheet">
+<link href="{{asset('build/assets/dashboard/css/style-dark.css')}}" rel="stylesheet">
+<!---Skinmodes css-->
+<link href="{{asset('build/assets/dashboard/css/skin-modes.css')}}" rel="stylesheet" />
+<link rel="stylesheet" href="{{asset('build/assets/dashboard/plugins/owl-carousel/owl.carousel.css')}}">
+<link href="{{asset('build/assets/dashboard/plugins/jqvmap/jqvmap.min.css')}}" rel="stylesheet">
